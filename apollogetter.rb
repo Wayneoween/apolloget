e705966a180e9d3fcfdffed1079c0e285acfdfac
Marius Schuller <marius.schuller@googlemail.com>
2013-12-08 00:55:11 +0100

    Cleanup, hacking@mafis was productive at least

--- apollogetter.rb
+++ apollogetter.rb
@@ -1,6 +1,7 @@
 require 'mechanize'
 require 'rubygems'
-require "net/http"
+require 'net/http'
+require 'pry'
 
 # This thing sucks a bit, because I don't want to use selenium
 # for clicking JS links.
@@ -14,47 +15,39 @@ require "net/http"
 # http://www.apolloarchive.com/apg_thumbnail-test.php?ptr=1&imageID=<IMGNAME>
 # to get the frame content where I can choose to download Standard or
 # sometimes high resolution images.
-#
-# URL Schemes:
-# Pre-Apollo:		http://www.hq.nasa.gov/office/pao/History/alsj/mercgem/mg-#{imgname}.jpg
-# Early-Apollo:		http://www.hq.nasa.gov/office/pao/History/alsj/misc/apmisc-#{imgname}.jpg
-# Apollo 1:			http://www.hq.nasa.gov/office/pao/History/alsj/a410/ap1-#{imgname}.jpg
-# Apollo 7:			http://www.hq.nasa.gov/office/pao/History/alsj/a410/ap7-#{imgname}.jpg
-# Apollo 8:			http://www.hq.nasa.gov/office/pao/History/alsj/a410/ap8-#{imgname}.jpg
-# Apollo 9:			http://www.hq.nasa.gov/office/pao/History/alsj/a410/ap9-#{imgname}.jpg
-# Apollo 10:		http://www.hq.nasa.gov/office/pao/History/alsj/a410/ap10-#{imgname}.jpg
-# Apollo 11:		http://www.hq.nasa.gov/office/pao/History/alsj/a11/ap11-#{imgname}.jpg
-# Apollo 12:		http://www.hq.nasa.gov/office/pao/History/alsj/a12/ap12-#{imgname}.jpg
-# Apollo 13:		http://www.hq.nasa.gov/office/pao/History/alsj/a13/ap13-#{imgname}.jpg
-# Apollo 14:		http://www.hq.nasa.gov/office/pao/History/alsj/a14/ap14-#{imgname}.jpg
-# Apollo 15:		http://www.hq.nasa.gov/office/pao/History/alsj/a15/ap15-#{imgname}.jpg
-# Apollo 16:		http://www.hq.nasa.gov/office/pao/History/alsj/a16/ap16-#{imgname}.jpg
-# Apollo 17:		http://www.hq.nasa.gov/office/pao/History/alsj/a17/ap17-#{imgname}.jpg
-#
-# XXX: I tested only the first and the last image for the name, but found out, that in between
-# there are images with AS16-#{imgname}.jpg, too...
-
 
 class ApolloGetter
   url = "http://www.apolloarchive.com/apollo_gallery.html"
   tableurl = "http://www.apolloarchive.com/apg_subject_index-test.php?gallery="
-  img_base_url = "http://www.hq.nasa.gov/office/pao/History/alsj/"
 
-  preapollo = ['mg', 'apmisc']
-  premoon = ['ap1', 'ap7', 'ap8', 'ap9', 'ap10']
-  moon = ['ap11', 'ap12', 'ap13', 'ap14', 'ap15', 'ap16', 'ap17']
-  postapollo = []
+  # http://www.apolloarchive.com/apg_thumbnail-test.php?ptr=<img id>&imageID=<name>
+  dlurl = "http://www.apolloarchive.com/apg_thumbnail-test.php?ptr="
+  dlurl_post = "&imageID="
+
+  missions = {'mg' => 'MG', 'apmisc' => 'EA', 'ap1' => '1', 'ap7' => '7',
+              'ap8' => '8', 'ap9' => '9', 'ap10' => '10', 'ap11' => '11',
+              'ap12' => '12', 'ap13' => '13', 'ap14' => '14',
+              'ap15' => '15', 'ap16' => '16', 'ap17' => '17',
+              'sv' => 'saturnv', 'pa' => 'PA'}
 
   linknames = []
-  imgnames = []
+  table = {}
+  resolutionlist = []
   geturls = []
+
+  stack = {}
+
+  # hash with the names as key, value is array of imgname
+  missions.each_key do |name|
+    stack[name] = []
+  end
 
   a = Mechanize.new
   a.get(url) do |hp|
 
     selector = a.click(hp.frame_with(:src => "apg_selector.html").click)
 
-    # Get all links in a
+    # Get all JS links from the Apollo overview
     selector.links.each do |l|
       linknames << l.href.match(/\(\'(.+)\',/)[1]
     end
@@ -62,43 +55,31 @@ class ApolloGetter
     # XXX: Get rid of search and magazines link
     linknames.pop(2)
 
-    # Get Apollo1 link table
-    table = a.get(tableurl+"1")
-    table.links.each do |l|
-      # probably better to use the href
-      # because all links have text, but
-      # only image links are Javascript links?
-      imgnames << l.text
+    missions.each do |name, number|
+      table[name] = a.get(tableurl+number)
+      table[name].links.each do |l|
+        stack[name] << l.text
+      end
+      # XXX: Get rid of not-image-links
+      stack[name].pop(6)
     end
 
-    # XXX: Get rid of not-img-links
-    imgnames.pop(6)
+#    stack.each do |indexnames, tables|
+#      FileUtils.mkdir_p name
+#      tables.each_with_index do |imgname, index|
+#        geturls << dlurl + "#{index+1}" + dlurl_post + imgname
+#      end
+#      Dir.chdir(name) do
+#        geturls.each do |l|
+#          a.get(l).save
+#        end
+#      end
+#    end
+
+    binding.pry
 
     # (<count> enries found in <Name> gallery)
-    # I like to use this count to verify that I got
-    # all names
-
-    # Now I have to know somehow which apollo mission I clicked on
-    # Also HighRes images do end with *HR.jpg, I want these instead of
-    # standard resolution.
-    imgnames.each do |l|
-      if l.downcase.match(/^ap\d/)
-        l = l.downcase.gsub(/id/, "ID")
-        geturls << img_base_url + "a410/" + "#{l}.jpg"
-      elsif l.downcase.match(/^as\d\d/)
-        # fix da URL
-      else
-        if l.match(/ID/)
-          l = l.downcase.gsub(/id/, "ID")
-        end
-        geturls << img_base_url + "a410/ap1-" + "#{l}.jpg"
-      end
-    end
-
-    pp table
-    pp imgnames
-    pp linknames
-    pp geturls
+    # I like to use this count to verify that I got all names
 
     # Just to see if there are all images available that we found
     # Worst case for HighRes images: test if regex on u "*HR.jpg" returns a 200 too
@@ -109,6 +90,5 @@ class ApolloGetter
     #	res = req.request_head(url.path)
     #	counter += 1 if res.code == "200"
     # end
-
   end
 end
